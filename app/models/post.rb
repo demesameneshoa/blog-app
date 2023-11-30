6263fa0243e8ac0ffe0976b544fa5e8fae1a7168
Demes Abera Ameneshoa <128508830+demesameneshoa@users.noreply.github.com>
2023-11-30 16:02:58 +0000

    Added Validation for user and posts models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   after_save :update_user_posts_counter
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   attribute :posts_counter, :integer, default: 0
   after_save :update_user_posts_counter
   def three_most_recent_posts
