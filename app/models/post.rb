8070cc7482c09c71c17ee2864b60ac49e3fd2127
Demes Abera Ameneshoa <128508830+demesameneshoa@users.noreply.github.com>
2023-11-30 11:32:41 +0000

    Added AfterSave ActiveRecord Callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  after_save :update_user_posts_counter
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   attribute :posts_counter, :integer, default: 0
+  after_save :update_user_posts_counter
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  def update_user_posts_counter
+    update(posts_counter: posts.count)
   end
 end
