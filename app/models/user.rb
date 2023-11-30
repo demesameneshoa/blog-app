3138aeeed9e1654e191880b86861317f1e1dbce6
Demes Abera Ameneshoa <128508830+demesameneshoa@users.noreply.github.com>
2023-11-30 07:18:51 +0000

    Created custom methods to fetch and update records

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+  attribute :posts_counter, :integer, default: 0
+  def three_most_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_206412) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_30_071032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_206412) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "posts_counter"
   end
 
   add_foreign_key "comments", "posts"
