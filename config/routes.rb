2d4fbf069be47e092085cb59900d652e417f0acf
Demes Abera Ameneshoa <128508830+demesameneshoa@users.noreply.github.com>
2023-12-04 19:50:11 +0000

    Added views templates and routes for all routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   # Get /users
-  get 'users', to: 'users#index' 
-  # Get /posts
-  get 'posts', to: 'posts#index' 
+  get '/users', to: 'users#index' 
+  # Get /users/id
+  get '/users/:id', to: 'users#show' 
+  # Get /users/id/posts
+  get '/users/:user_id/posts', to: 'posts#index' 
+  # Get /users/user_id/posts/id
+  get '/users/:user_id/posts/:id', to: 'posts#show' 
 end
