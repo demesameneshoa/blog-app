f1ca33c721d37dcdaa1e41dbeb70105bd12d1a58
Demes Abera Ameneshoa <128508830+demesameneshoa@users.noreply.github.com>
2023-12-05 08:10:06 +0000

    Added controller spec tests for posts

--- spec/requests/controllers_specs_spec.rb
+++ spec/requests/controllers_specs_spec.rb
@@ -16,4 +16,21 @@ RSpec.describe 'ControllersSpecs', type: :request do
       expect(response.body).to include('Single User Page')
     end
   end
+  describe 'PostsController' do
+    it 'renders the posts index page for a user' do
+      user = create(:user)
+      get "/users/#{user.id}/posts"
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:index)
+      expect(response.body).to include('All Posts Page')
+    end
+    it 'renders the posts show page for a selected post' do
+      user = create(:user) # Assuming you have FactoryBot configured
+      post = create(:post, author: user)
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:show)
+      expect(response.body).to include('Single Post Page')
+    end
+  end
 end
