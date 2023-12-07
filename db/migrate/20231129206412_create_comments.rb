51b6f80780a2b5e2bf85c8bc803d161aafca5976
Demes Abera Ameneshoa <128508830+demesameneshoa@users.noreply.github.com>
2023-12-07 05:25:17 +0000

    Created users view as per the wireframe

--- db/migrate/20231129206412_create_comments.rb
+++ db/migrate/20231129206412_create_comments.rb
@@ -4,6 +4,7 @@ class CreateComments < ActiveRecord::Migration[7.1]
       t.references :author, foreign_key: { to_table: :users }, null: false
       t.references :post, null: false, foreign_key: true
       t.text :text
+      t.integer :comments_counter, default: 0
 
       t.timestamps
     end
