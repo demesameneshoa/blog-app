66fe99ee7002ef009cd3727ab7d9242e9892f403
Demes Abera Ameneshoa <128508830+demesameneshoa@users.noreply.github.com>
2023-12-07 13:07:29 +0000

    Resolved rubocop offenses and stylelint errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,19 @@
 class CommentsController < ApplicationController
-    def create
-      @user = User.find(params[:user_id])
-      @post = Post.find(params[:post_id])
-      @comment = @post.comments.build(comment_params)
-      @comment.author = @user
-  
-      if @comment.save
-        redirect_to user_post_path(@user, @post), notice: 'Comment added!'
-      else
-        render 'posts/show'
-      end
-    end
-  
-    private
-  
-    def comment_params
-      params.require(:comment).permit(:text)
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.author = @user 
+    if @comment.save
+      redirect_to user_post_path(@user, @post), notice: 'Comment added!'
+    else
+      render 'posts/show'
     end
   end
-  
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
