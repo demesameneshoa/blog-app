1fddc43763930b508e92bad1cc588457bc68f9b4
demesameneshoa <demesameneshoa@gmail.com>
2023-12-13 00:59:41 +0300

    Updated the user index to to fulfill all the requirements

--- spec/views/user_index_spec.rb
+++ spec/views/user_index_spec.rb
@@ -1,30 +1,35 @@
 require 'rails_helper'
-
 RSpec.describe Post, type: :system do
   describe 'user index page' do
-    before do
-      create(:user, name: 'User1', posts_counter: 3)
-      create(:user, name: 'User2', posts_counter: 5)
+    before(:each) do
+      @users = create_list(:user, 5)
       visit users_path
-    end
-    # I can see the username of all other users.
+    end    # I can see the username of all other users.
     it 'displays the username of all other users' do
-      expect(page).to have_content('User1')
-      expect(page).to have_content('User2')
-    end
-    # I can see the profile picture for each user.
-    it 'displays the profile picutres of the users' do
-      expect(page).to have_selector('img', count: 2)
-    end
-    # I can see the number of posts each user has written.
+      @users.each do |user|
+        expect(page).to have_content(user.name)
+      end
+    end    # I can see the profile picture for each user.
+    it 'displays the profile pictures of the users' do
+      @users.each do |user|
+        expect(page).to have_css("img[src*='#{user.photo}']")
+      end
+    end    # I can see the number of posts each user has written.
     it 'displays the number of posts each user has written' do
-      expect(page).to have_content('Number of posts: 3', count: 1)
-      expect(page).to have_content('Number of posts: 5', count: 1)
+      @users.each do |user|
+        expect(page).to have_content(user.posts_counter)
+      end
     end
-    # When I click on a user, I am redirected to that user's show page.
-    it 'when user is clicked it redirects to that users show page' do
-      click_link 'User1'
-      expect(page).to have_current_path(user_path(User.find_by(name: 'User1')))
+  end  
+  describe 'when a user is clicked' do
+    # I can click on a user's name to go to their show page.
+    it 'it should redirects to that users show page' do
+      # ? Factory Bot creates 5 users, so we need to take one of them to test. since all o them share
+      # ? the same data (e.g. name).
+      user = create(:user) # ! so just create a brand new one otherwise if iterates through all users we get 'ambiguous match' error.      
+      visit users_path
+      click_link user.name
+      expect(page).to have_current_path(user_path(user))
     end
   end
 end
