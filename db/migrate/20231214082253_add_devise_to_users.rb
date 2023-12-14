6273712f1989e661850e5f5519d1e7798e06148d
demesameneshoa <demesameneshoa@gmail.com>
2023-12-14 14:09:45 +0300

    fix: implemented confirmable feature for user model

--- db/migrate/20231214082253_add_devise_to_users.rb
+++ db/migrate/20231214082253_add_devise_to_users.rb
@@ -32,6 +32,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :confirmable
+
       # Uncomment below if timestamps were not included in your original model.
       # t.timestamps null: false
     end
